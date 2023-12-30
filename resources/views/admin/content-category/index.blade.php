6986d9112b3b301706bc06cf75cab805073a1649
Halil Hodzic <habetech@gmail.com>
2023-12-30 22:38:53 +0100

    update

--- resources/views/admin/content-category/index.blade.php
+++ resources/views/admin/content-category/index.blade.php
@@ -5,7 +5,12 @@
             <div class="panel flex items-center overflow-x-auto whitespace-nowrap p-3 text-primary">
 
                 @can('content_category_create')
-                    <a class="btn btn-primary" href="{{ route('admin.content-categories.create') }}">
+                    <a class="btn btn-primary getMyFormModal" href="{{ route('admin.content-categories.create') }}"
+                    data-title="{{ trans('global.add') }} {{ trans('cruds.contentCategory.title_singular') }}"
+                    data-url="{{ route('admin.form.getMyForm') }}"
+                    data-form-name="{{ encrypt('Add Category') }}"
+                    data-id="{{ encrypt('0') }}">
+                
                         {{ trans('global.add') }} {{ trans('cruds.contentCategory.title_singular') }}
                     </a>
                 @endcan
