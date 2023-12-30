930430819ae5ce993f6c9a1ffd989062ec81696e
Adis <klobodanovic.adis.21@size.ba>
2023-12-30 09:52:20 +0100

    Permissions Section Full

--- resources/views/admin/permission/create.blade.php
+++ resources/views/admin/permission/create.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="card bg-blueGray-100">
         <div class="card-header">
-            <div class="card-header-container">
+            <div class="card-header-container text-center">
                 <h6 class="card-title">
                     {{ trans('global.create') }}
                     {{ trans('cruds.permission.title_singular') }}
--- resources/views/admin/permission/index.blade.php
+++ resources/views/admin/permission/index.blade.php
@@ -2,22 +2,25 @@
 @section('content')
 <div class="row">
     <div class="card bg-white">
-        <div class="card-header border-b border-blueGray-200">
-            <div class="card-header-container">
+        <div class="card-header border-b border-blueGray-200 flex items-center justify-center">
+            <div class="card-header-container flex items-center space-x-3">
+                {{-- Naslov --}}
                 <h6 class="card-title">
                     {{ trans('cruds.permission.title_singular') }}
                     {{ trans('global.list') }}
+                     <span class="ml-1">:</span>
                 </h6>
 
+                {{-- Dugme za dodavanje --}}
                 @can('permission_create')
-                    <a class="btn btn-indigo" href="{{ route('admin.permissions.create') }}">
+                    <a class="btn bg-primary bg-indigo-600 text-white font-bold py-2 px-4 rounded-lg mb-1" href="{{ route('admin.permissions.create') }}">
                         {{ trans('global.add') }} {{ trans('cruds.permission.title_singular') }}
                     </a>
                 @endcan
             </div>
         </div>
         @livewire('permission.index')
-
     </div>
 </div>
+
 @endsection
