23d312965a446dea8eb112a073f7162e1b5c70f5
Halil Hodzic <habetech@gmail.com>
2024-01-11 16:27:28 +0100

    update

--- resources/views/admin/content-page/edit.blade.php
+++ resources/views/admin/content-page/edit.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.admin')
 @section('content')
-<div class="row">
-    <div class="card bg-blueGray-100">
-        <div class="card-header">
-            <div class="card-header-container">
-                <h6 class="card-title">
-                    {{ trans('global.edit') }}
-                    {{ trans('cruds.contentPage.title_singular') }}:
-                    {{ trans('cruds.contentPage.fields.id') }}
-                    {{ $contentPage->id }}
-                </h6>
+    <div class="animate__animated p-6" :class="[$store.app.animation]">
+        <div x-data="exportTable">
+            <div class="panel">
+           
+                <div class="mb-5 flex items-center justify-between">
+                    <h5 class="text-lg font-semibold dark:text-white-light">  {{ trans('global.edit') }} /page/{{ $contentPage->id }}</h5>
+                    <a class="font-semibold hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-600" href="javascript:;" @click="toggleCode('code13')">
+                        <span class="flex items-center">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ltr:mr-2 rtl:ml-2">
+                                <path d="M17 7.82959L18.6965 9.35641C20.239 10.7447 21.0103 11.4389 21.0103 12.3296C21.0103 13.2203 20.239 13.9145 18.6965 15.3028L17 16.8296" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
+                                <path opacity="0.5" d="M13.9868 5L10.0132 19.8297" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
+                                <path d="M7.00005 7.82959L5.30358 9.35641C3.76102 10.7447 2.98975 11.4389 2.98975 12.3296C2.98975 13.2203 3.76102 13.9145 5.30358 15.3028L7.00005 16.8296" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
+                            </svg>
+                            Code
+                        </span>
+                    </a>
+                </div>
+
+
+                <div class="card-body">
+                    @livewire('content-page.edit', [$contentPage])
+                </div>
             </div>
         </div>
-
-        <div class="card-body">
-            @livewire('content-page.edit', [$contentPage])
-        </div>
     </div>
-</div>
 @endsection
