6ac989491346fed55d462e81492078d3c9895484
Halil Hodzic <habetech@gmail.com>
2024-01-11 22:34:46 +0100

    update

---
+++ app/Models/Bid.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bid extends Model
+{
+    use HasFactory;
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Ako imate povezane razgovore s ponudama
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+}
