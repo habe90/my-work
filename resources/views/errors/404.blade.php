654fe072062051841445930f56b2bfab779f9a36
Halil Hodzic <habetech@gmail.com>
2024-03-19 10:43:09 +0100

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,17 +1,19 @@
-{{-- resources/views/errors/404.blade.php --}}
-@extends('layouts.app')
 
-@section('title', 'Stranica nije pronađena')
+@extends('frontend.layouts.front')
+
+@section('title', 'Seite nicht gefunden')
 
 @section('content')
-<div class="d-flex align-items-center justify-content-center vh-100">
-    <div class="text-center">
-        <h1 class="display-1 fw-bold">404</h1>
-        <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
-        <p class="lead">
-            The page you’re looking for doesn’t exist.
-          </p>
-        <a href="{{ url('/') }}" class="btn btn-primary">Go Home</a>
+<div class="container">
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold" style="font-size: 6rem;">404</h1>
+            <p class="fs-1"> <span class="text-danger">Hoppla!</span> Seite nicht gefunden.</p>
+            <p class="fs-3">
+                Die Seite, nach der Sie suchen, existiert nicht.
+            </p>
+            <a href="{{ url('/') }}" class="btn btn-primary btn-lg">Zur Startseite</a>
+        </div>
     </div>
 </div>
 @endsection
