add5e227d60b65651bbf93f50f686c955cbd5f0f
Halil Hodzic <habetech@gmail.com>
2024-03-28 11:01:02 +0100

    Update index.blade.php

--- resources/views/admin/invoice/index.blade.php
+++ resources/views/admin/invoice/index.blade.php
@@ -10,7 +10,6 @@
                     data-url="{{ route('admin.form.getMyForm') }}"
                     data-form-name="{{ encrypt('Add Inovice') }}"
                     data-id="{{ encrypt('0') }}">
-                    
                         {{ trans('global.add_inovice') }} 
                     </a>
                 @endcan
@@ -18,8 +17,13 @@
             </div>
             <div class="panel mt-6">
                 @livewire('invoices-table')
+            </div>
 
-
+            {{-- Uputstvo za Cron komandu --}}
+            <div class="mt-6">
+                <h3 class="text-lg font-medium leading-6 text-gray-900">Uputstvo za Cron komandu</h3>
+                <p class="mt-1 text-sm text-gray-600">Koristite ovu Cron komandu da automatski pokrenete zadatak generisanja faktura na poslednji dan svakog mjeseca u 17:00 sati.</p>
+                <pre class="bg-gray-100 rounded-md p-4 mt-2"><code>0 17 * * * cd /putanja-do-vasheg-projekta && php artisan schedule:run >> /dev/null 2>&1</code></pre>
             </div>
         </div>
     </div>
