9ccce7b5c09d4f88eb26cebf1319bdc03069bce2
Halil Hodzic <habetech@gmail.com>
2024-03-29 11:04:08 +0100

    Update UserProfileController.php

--- app/Http/Controllers/Auth/UserProfileController.php
+++ app/Http/Controllers/Auth/UserProfileController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Admin\FormController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Encrypt;
 
 class UserProfileController extends Controller
 {
@@ -12,6 +14,25 @@ class UserProfileController extends Controller
     {
         $this->authorize('auth_profile_edit');
 
-        return view('profile.show');
+        // ID trenutno prijavljenog korisnika
+        $userId = encrypt(Auth::user()->id);
+        $accountId = encrypt(Auth::user()->account_id); // Ovo pretpostavlja da 'account_id' postoji u User modelu
+
+        // Učitavanje forme za uređivanje profila
+        $editProfileFormRequest = new Request([
+            'recordID' => $userId,
+            'formName' => encrypt('Edit Admin Profile'),
+        ]);
+        $editProfileForm = app(FormController::class)->loadMyForm($editProfileFormRequest);
+
+        // Učitavanje forme za promjenu lozinke
+        $changePasswordFormRequest = new Request([
+            'recordID' => $accountId,
+            'formName' => encrypt('Change password'),
+        ]);
+        $changePasswordForm = app(FormController::class)->loadMyForm($changePasswordFormRequest);
+
+        // Prikazivanje view-a sa formama
+        return view('profile.show', compact('editProfileForm', 'changePasswordForm'));
     }
 }
