89777196866748394fdef46047391e3eab5c876e
Halil Hodzic <habetech@gmail.com>
2024-03-29 11:19:01 +0100

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -83,9 +83,35 @@
                     </li>
                 </ul>
                 <template x-if="tab === 'home'">
-                    {{!! $editProfileForm !!}}
-                    @livewire('update-profile-information-form')
-                    @livewire('update-password-form')
+                    <div>
+                        <form wire:submit.prevent="updateProfileInformation"
+                            class="mb-5 rounded-md border border-[#ebedf2] bg-white p-4 dark:border-[#191e3a] dark:bg-[#0e1726]">
+                            <h6 class="mb-5 text-lg font-bold"> {{ __('global.profile_information') }}</h6>
+                            <div class="flex flex-col sm:flex-row">
+                                {{-- ...ostatak vašeg koda... --}}
+                    
+                                {{-- Ovdje ubacujete vašu dinamički generisanu formu --}}
+                                {!! $editProfileForm !!}
+                    
+                                {{-- Ostatak vaše forme --}}
+                                <div class="form-group px-4 flex items-center">
+                                    <button class="btn btn-indigo mr-3">
+                                        {{ __('global.save') }}
+                                    </button>
+                    
+                                    <div x-data="{ shown: false, timeout: null }" x-init="@this.on('saved', () => { clearTimeout(timeout);
+                                        shown = true;
+                                        timeout = setTimeout(() => { shown = false }, 2000); })"
+                                        x-show.transition.out.opacity.duration.1500ms="shown" x-transition:leave.opacity.duration.1500ms
+                                        class="mt-3 sm:col-span-2" style="display: none;">
+                                        {{ __('global.saved') }}
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    
+                  
                 </template>
                 <template x-if="tab === 'payment-details'">
                     <div>
