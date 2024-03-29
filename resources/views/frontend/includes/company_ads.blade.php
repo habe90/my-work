878d555a2f2a41db800de6ad13d4d88000de6ef9
Halil Hodzic <habetech@gmail.com>
2024-03-29 17:00:11 +0100

    sda

--- resources/views/frontend/includes/company_ads.blade.php
+++ resources/views/frontend/includes/company_ads.blade.php
@@ -47,9 +47,7 @@
         </div>
     </div>
 </section>
-
-
-
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function(){
     $('.slick-slider-container').slick({
