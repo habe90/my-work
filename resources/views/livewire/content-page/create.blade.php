f1ddf34bb7845fdcd81ededd05eb3a89aed2ac56
Halil Hodzic <habetech@gmail.com>
2024-03-30 12:09:14 +0100

    update create page

--- resources/views/livewire/content-page/create.blade.php
+++ resources/views/livewire/content-page/create.blade.php
@@ -90,41 +90,4 @@
     </div>
 </form>
 
-<!-- JavaScript kod za inicijalizaciju biblioteka -->
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        // NiceSelect inicijalizacija
-        var selects = document.querySelectorAll(".selectize");
-        selects.forEach(function(select) {
-            NiceSelect.bind(select);
-        });
 
-        // EasyMDE inicijalizacija za polje 'page_text'
-        new EasyMDE({
-            element: document.getElementById('mde-page_text'),
-            autosave: {
-                enabled: true,
-                delay: 1000,
-                uniqueId: 'contentPage.page_text'
-            }
-        });
-
-        // EasyMDE inicijalizacija za polje 'excerpt'
-        new EasyMDE({
-            element: document.getElementById('mde-excerpt'),
-            autosave: {
-                enabled: true,
-                delay: 1000,
-                uniqueId: 'contentPage.excerpt'
-            }
-        });
-
-        // FileUploadWithPreview inicijalizacija za 'featured_image'
-        new FileUploadWithPreview('myFirstImage', {
-            images: {
-                baseImage: 'assets/images/file-preview.png',
-                backgroundImage: '',
-            },
-        });
-    });
-</script>
