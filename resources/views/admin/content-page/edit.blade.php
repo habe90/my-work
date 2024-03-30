3c42ccbdfb8ba27d2d6c206ba9fe13f1d996ec47
Halil Hodzic <habetech@gmail.com>
2024-03-30 15:24:30 +0100

    Update edit.blade.php

--- resources/views/admin/content-page/edit.blade.php
+++ resources/views/admin/content-page/edit.blade.php
@@ -1,37 +1,24 @@
 @extends('layouts.admin')
 @section('content')
-       <div class="animate__animated p-6" :class="[$store.app.animation]">
-        <div x-data="exportTable">
-            <div class="panel">
-           
-                <div class="mb-5 flex items-center justify-between">
-                    <h5 class="text-lg font-semibold dark:text-white-light">  {{ trans('global.edit') }} /page/{{ $contentPage->id }}</h5>
-                    <a class="font-semibold hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-600" href="javascript:;" @click="toggleCode('code13')">
-                        <a href="{{ route('admin.content-pages.index') }}" class="btn btn-secondary">
-                            {{ trans('global.cancel') }}
-                        </a>
+    <div class="animate__animated p-6" :class="[$store.app.animation]">
+
+        <div class="panel">
+
+            <div class="mb-5 flex items-center justify-between">
+                <h5 class="text-lg font-semibold dark:text-white-light"> {{ trans('global.edit') }}
+                    /page/{{ $contentPage->id }}</h5>
+                <a class="font-semibold hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-600" href="javascript:;"
+                    @click="toggleCode('code13')">
+                    <a href="{{ route('admin.content-pages.index') }}" class="btn btn-secondary">
+                        {{ trans('global.cancel') }}
                     </a>
-                </div>
-            
-                <div class="card-body">
-                    @livewire('content-page.edit', [$contentPage])
-                </div>
+                </a>
+            </div>
+
+            <div class="card-body">
+                @livewire('content-page.edit', [$contentPage])
             </div>
         </div>
+
     </div>
-    
-    <script>
-        var quill = new Quill('#editor', {
-            theme: 'snow',
-        });
-        var toolbar = quill.container.previousSibling;
-        toolbar.querySelector('.ql-picker').setAttribute('title', 'Font Size');
-        toolbar.querySelector('button.ql-bold').setAttribute('title', 'Bold');
-        toolbar.querySelector('button.ql-italic').setAttribute('title', 'Italic');
-        toolbar.querySelector('button.ql-link').setAttribute('title', 'Link');
-        toolbar.querySelector('button.ql-underline').setAttribute('title', 'Underline');
-        toolbar.querySelector('button.ql-clean').setAttribute('title', 'Clear Formatting');
-        toolbar.querySelector('[value=ordered]').setAttribute('title', 'Ordered List');
-        toolbar.querySelector('[value=bullet]').setAttribute('title', 'Bullet List');
-    </script>
 @endsection
