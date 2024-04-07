fc29e891d9a749c786d95a9c931825749397db43
Halil Hodzic <habetech@gmail.com>
2024-04-07 16:09:03 +0200

    Update postal-code-lookup.blade.php

--- resources/views/livewire/postal-code-lookup.blade.php
+++ resources/views/livewire/postal-code-lookup.blade.php
@@ -2,7 +2,7 @@
     <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="postal_code">
         {{ __('global.postal_code') }}
     </label>
-    <input id="postal_code" wire:model="postalCode" type="text" class="input-with-city"
+    <input id="postal_code" wire:model="postalCode" type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full input-with-city"
         placeholder="{{ __('global.postal_code') }}" required autocomplete="postal-code" />
     @error('postal_code')
         <div class="text-red-500">
