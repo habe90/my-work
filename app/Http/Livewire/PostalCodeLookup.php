f7330f76c9e238004d5a561d1b95a8c0510340b9
Halil Hodzic <habetech@gmail.com>
2024-04-07 16:17:09 +0200

    Update PostalCodeLookup.php

--- app/Http/Livewire/PostalCodeLookup.php
+++ app/Http/Livewire/PostalCodeLookup.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class PostalCodeLookup extends Component
 {
@@ -11,10 +12,17 @@ class PostalCodeLookup extends Component
 
     public function updatedPostalCode($value)
     {
-        if ($value === '10115') {
-            $this->postalCode = $value . ' | Berlin';
-        } else {
-            $this->postalCode = $value . ' | Unknown';
+        if (strlen($value) === 5) {
+            $response = Http::get("https://api.postcodes.io/postcodes/{$value}/autocomplete");
+    
+            if ($response->successful()) {
+                $data = $response->json();
+                // You'll need to parse this $data to find the city or area name
+                // Assume we have the city name in $data['result'][0]
+                $this->postalCode = $value . ' | ' . $data['result'][0];
+            } else {
+                $this->postalCode = $value . ' | Unknown';
+            }
         }
     }
     
