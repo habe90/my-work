33c26dcb5b50e6651823ca01b5fedeffea5bfb47
Halil Hodzic <habetech@gmail.com>
2024-04-07 11:26:48 +0200

    Update bookmarked-jobs.blade.php

--- resources/views/livewire/bookmarked-jobs.blade.php
+++ resources/views/livewire/bookmarked-jobs.blade.php
@@ -1,21 +1,21 @@
 <div>
-    @forelse($bookmarkedJobs as $job)
+    @forelse($bookmarkedJobs as $bookmark) 
 
         <div class="_list_jobs_wraps mng_list shadow_0 border">
             <div class="_list_jobs_f1ex first">
                 <div class="_list_110">
                     <div class="_list_110_thumb">
                         <!-- Ispravljena ruta za detaljan prikaz posla -->
-                        <a href="{{ route('jobs.show', $job->id) }}"><img src="{{ $job->featured_image ?: 'https://via.placeholder.com/250x250' }}" class="img-fluid" alt="{{ $job->title }}"></a>
+                        <a href="{{ route('jobs.show', $bookmark->job->id) }}"><img src="{{ $bookmark->job->featured_image ?: 'https://via.placeholder.com/250x250' }}" class="img-fluid" alt="{{ $bookmark->job->title }}"></a>
                     </div>
                     <div class="_list_110_caption">
-                        <h4 class="_jb_title"><a href="{{ route('jobs.show', $job->id) }}">{{ $job->title }}</a></h4>
+                        <h4 class="_jb_title"><a href="{{ route('jobs.show', $bookmark->job->id) }}">{{ $bookmark->job->title }}</a></h4>
                         <ul class="_grouping_list">
                             <!-- Ostali podaci o poslu -->
-                            <li><span><i class="ti-briefcase"></i>{{ $job->service_category_id }}</span></li>
-                            <li><span><i class="ti-credit-card"></i>{{ $job->salary_range }}</span></li>
-                            <li><span><i class="ti-location-pin"></i>{{ $job->location }}</span></li>
-                            <li><span><i class="ti-timer"></i>{{ $job->created_at->diffForHumans() }}</span></li>
+                            <li><span><i class="ti-briefcase"></i>{{ $bookmark->job->service_category_id }}</span></li>
+                            <li><span><i class="ti-credit-card"></i>{{ $bookmark->job->salary_range }}</span></li>
+                            <li><span><i class="ti-location-pin"></i>{{ $bookmark->job->location }}</span></li>
+                            <li><span><i class="ti-timer"></i>{{ $bookmark->job->created_at->diffForHumans() }}</span></li>
                         </ul>
                     </div>
                 </div>
