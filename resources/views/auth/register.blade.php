6def15001a25fa74bf15c9d2b771d623c7b219ff
Halil Hodzic <habetech@gmail.com>
2024-04-07 16:49:25 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,7 +52,7 @@
                                 <div class="relative w-full mb-3">
                                     <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                                         for="address">
-                                        {{ __('global.postal_code') }}
+                                        {{ __('global.city') }}
                                     </label>
                                     <input id="address" name="address" type="text"
                                         class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
