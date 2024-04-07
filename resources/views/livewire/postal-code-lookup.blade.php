8c9febf7bea1fb0574115026c64340f13a72befc
Halil Hodzic <habetech@gmail.com>
2024-04-07 16:33:47 +0200

    Update postal-code-lookup.blade.php

--- resources/views/livewire/postal-code-lookup.blade.php
+++ resources/views/livewire/postal-code-lookup.blade.php
@@ -1,8 +1,8 @@
 <div class="relative w-full mb-3">
-    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="postal_code">
+    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="address">
         {{ __('global.postal_code') }}
     </label>
-    <input id="postal_code" wire:model.lazy="postalCode" type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full input-with-city"
+    <input id="address" wire:model.lazy="address" type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full input-with-city"
     placeholder="{{ __('global.postal_code') }}" required autocomplete="postal-code" name="address" />
 
     @error('postal_code')
