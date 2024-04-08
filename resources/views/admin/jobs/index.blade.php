cbf20db9c38d4d5816daae4a6c75d19c4f05bbb3
Halil Hodzic <habetech@gmail.com>
2024-04-08 11:01:20 +0200

    Update index.blade.php

--- resources/views/admin/jobs/index.blade.php
+++ resources/views/admin/jobs/index.blade.php
@@ -6,7 +6,7 @@
 
                 @can('content_page_create')
                     <a class="btn btn-primary getMyFormModal" href="{{ route('admin.content-pages.create') }}"
-                    data-title="{{ __('global.add_page') }}"
+                    data-title="{{ __('global.add_job') }}"
                     data-url="{{ route('admin.form.getMyForm') }}"
                     data-form-name="{{ encrypt('Add Job') }}"
                     data-id="{{ encrypt('0') }}">
