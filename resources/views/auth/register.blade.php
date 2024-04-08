837b77dacefeb3f97963de81cb28ec4654e56ae1
Halil Hodzic <habetech@gmail.com>
2024-04-08 12:19:09 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<style>.logo-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
     <section class="relative w-full h-full py-40 min-h-screen">
         <div class="container mx-auto px-4 h-full">
             <div class="flex content-center items-center justify-center h-full">
@@ -8,15 +15,18 @@
                     <div
                         class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
                         <div class="rounded-t mb-0 px-6 py-6">
-                            <div class="text-center mb-3">
-                                <a class="nav-brand static-logo text-center" href="/"><img src="frontend/img/logo-my-work.png"
-                                    width="180" class="logo" alt="" /></a>
+                            <div class="text-center mb-3 logo-center">
+                                <a class="nav-brand static-logo" href="/">
+                                    <img src="frontend/img/logo-my-work.png" width="180" class="logo" alt="" />
+                                </a>
                                 <h6 class="text-blueGray-500 text-sm font-bold">
                                     {{ __('global.register') }}
                                 </h6>
                             </div>
                             <hr class="mt-6 border-b-1 border-blueGray-300" />
                         </div>
+                        
+                        
                         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
                             <form method="POST" action="{{ route('register') }}">
                                 @csrf
