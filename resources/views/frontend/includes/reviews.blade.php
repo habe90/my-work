9a954b76ee54ea15c50f960004e47f115588faa9
Halil Hodzic <habetech@gmail.com>
2024-04-18 18:14:55 +0200

    dd

--- resources/views/frontend/includes/reviews.blade.php
+++ resources/views/frontend/includes/reviews.blade.php
@@ -1,46 +1,44 @@
-	<!-- ============================ Featured Themes Start ==================================== -->
-    <section class="light-w">
-				<div class="container">
-					<div class="row justify-content-center">
-						<div class="col-lg-7 col-md-9">
-							<div class="sec-heading">
-								<h2>Was die Leute <span class="theme-cl-2">sagen</span></h2>
-								<p>Entdecken Sie die Erfahrungen unserer Kunden und wie unsere Dienstleistungen dazu beigetragen haben, ihre Projekte erfolgreich umzusetzen.</p>
-							</div>
-						</div>
-					</div>
-					
-					
-					<div class="row">
-						<div class="col-lg-12 col-md-12">
-							<div class="owl-carousel owl-theme middle-arrow-hover" id="theme-slide">
-							
-									@foreach ($reviews as $review)
-									<!-- Single Review -->
-									<div class="item">
-										<div class="smart-testimonials">
-											<div class="smart-testi-thumb">
-												<img src="{{ $review->image }}" class="img-fluid" alt="" />
-												<span class="cipt bg-success"><i class="fa fa-quote-left"></i></span>
-											</div>
-											<div class="smart-testimonials-content">
-												<div class="smart-tes-content">
-													<p>{{ $review->description }}</p>
-												</div>
-												<div class="st-author-info">
-													<h4 class="st-author-title">{{ $review->name }}</h4>
-													<span class="st-author-subtitle theme-cl">{{ $review->position }}</span>
-												</div>
-											</div>
-										</div>
-									</div>
-								@endforeach
-									
-							</div>
-						</div>
-					</div>
-					
-				</div>
-				
-			</section>
-			<!-- ============================ Featured Themes End ==================================== -->
+<!-- ============================ Featured Themes Start ==================================== -->
+<section class="light-w">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-md-9">
+                <div class="sec-heading">
+                    <h2>{{ __('front.what_people_say') }}</h2>
+                    <p>{{ __('front.customer_experiences') }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="owl-carousel owl-theme middle-arrow-hover" id="theme-slide">
+                    @foreach ($reviews as $review)
+                    <!-- Single Review -->
+                    <div class="item">
+                        <div class="smart-testimonials">
+                            <div class="smart-testi-thumb">
+                                <img src="{{ $review->image }}" class="img-fluid" alt="" />
+                                <span class="cipt bg-success"><i class="fa fa-quote-left"></i></span>
+                            </div>
+                            <div class="smart-testimonials-content">
+                                <div class="smart-tes-content">
+                                    <p>{{ $review->description }}</p>
+                                </div>
+                                <div class="st-author-info">
+                                    <h4 class="st-author-title">{{ $review->name }}</h4>
+                                    <span class="st-author-subtitle theme-cl">{{ $review->position }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+<!-- ============================ Featured Themes End ==================================== -->
