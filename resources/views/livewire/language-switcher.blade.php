e04555e7825adcc1d52f29a9b5a754e224ce0669
Halil Hodzic <habetech@gmail.com>
2024-04-18 15:25:50 +0200

    update

--- resources/views/livewire/language-switcher.blade.php
+++ resources/views/livewire/language-switcher.blade.php
@@ -1,12 +1,12 @@
 <div class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-        <img src="/path/to/flags/{{ $currentLanguage }}.png" alt="{{ $currentLanguage }}" style="width: 20px;"> 
+        <img src="/frontend/img/{{ $currentLanguage }}.png" alt="{{ $currentLanguage }}" style="width: 20px;"> 
         {{ $currentLanguage }}
     </a>
     <div class="dropdown-menu" aria-labelledby="languageDropdown">
         @foreach($languages as $language)
             <a class="dropdown-item" wire:click="changeLocale('{{ $language['short_code'] }}')" href="#">
-                <img src="/path/to/flags/{{ $language['short_code'] }}.png" alt="{{ $language['title'] }}" style="width: 20px;"> 
+                <img src="/frontend/img/{{ $language['short_code'] }}.png" alt="{{ $language['title'] }}" style="width: 20px;"> 
                 {{ $language['title'] }}
             </a>
         @endforeach
