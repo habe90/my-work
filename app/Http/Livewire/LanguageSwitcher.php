20a88785a2f9a598411489778b68437eb38ae61a
Halil Hodzic <habetech@gmail.com>
2024-04-18 18:08:35 +0200

    update

--- app/Http/Livewire/LanguageSwitcher.php
+++ app/Http/Livewire/LanguageSwitcher.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use Illuminate\Support\Facades\Session;
 
 class LanguageSwitcher extends Component
 {
@@ -13,32 +12,16 @@ class LanguageSwitcher extends Component
 
     public function mount()
     {
-        // Provjera da li postoji jezik u sesiji ili kolačiću
-        $locale = session('locale', request()->cookie('locale', config('app.locale')));
-    
-        $this->languages = config('project.supported_languages');
-        $this->currentLanguage = $locale;
-    
-        // Postavljanje jezika aplikacije na osnovu odabrane vrijednosti
-        app()->setLocale($this->currentLanguage);
+        $this->languages       = config('project.supported_languages');
+        $this->currentLanguage = app()->getLocale();
     }
-    
 
     public function changeLocale(string $localeCode)
     {
-        if (auth()->check()) {
-            auth()->user()->update(['locale' => $localeCode]);
-        } else {
-            session(['locale' => $localeCode]);
-            cookie()->queue('locale', $localeCode, 43200); // Traje 30 dana
-        }
-    
-        app()->setLocale($localeCode);
-        session()->flash('locale', $localeCode); // Dodajemo flash sesiju za promjenu jezika
-    
-        $this->emit('localeChanged'); // Emitujemo događaj za osvježavanje stranice
+        auth()->user()->update(['locale' => $localeCode]);
+
+        return redirect(request()->header('Referer'));
     }
-    
 
     public function render()
     {
