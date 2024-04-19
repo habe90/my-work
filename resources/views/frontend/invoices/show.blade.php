854b8abe3c65855e7cb8e96bba57057bf22848b8
Halil Hodzic <habetech@gmail.com>
2024-04-19 22:47:06 +0200

    Update show.blade.php

--- resources/views/frontend/invoices/show.blade.php
+++ resources/views/frontend/invoices/show.blade.php
@@ -47,16 +47,13 @@
 
                                 <div class="_dashboard_content_body p-0">
 
-                                    <div
-                                        class="receipt-main {{ $invoice->status ? 'paid' : 'not-paid' }}  col-xs-10 col-sm-10 col-md-12 col-xs-offset-1 col-sm-offset-1 col-md-offset-3">
+                                    <div class="receipt-main {{ $invoice->getStatusClassAttribute() }} col-xs-10 col-sm-10 col-md-12 col-xs-offset-1 col-sm-offset-1 col-md-offset-3">
 
                                         <!-- Prikaz rednog broja fakture -->
                                         <div class="badge-container">
-                                            
-                                            <span class="badge {{ $invoice->status ? 'badge-success' : 'badge-danger' }}">
-                                                {{ $invoice->status ? __('global.paid') : __('global.not_paid') }}
+                                            <span class="badge {{ $invoice->getStatusClassAttribute() }}">
+                                                {{ __('global.' . $invoice->status) }}  
                                             </span>
-                                            
                                         </div>
 
                                         <div class="receipt-header receipt-header-mid">
