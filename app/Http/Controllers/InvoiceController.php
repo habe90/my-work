c7edcfbd1e71c2169f870de168fcb8870cdb314f
Halil Hodzic <habetech@gmail.com>
2024-04-19 19:38:05 +0200

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\SuccessfulJob;
 use App\Models\Invoice;
 use Auth;
-use Carbon\Carbon;
+use Carbon\Carbon; 
 use PDF;
 
 class InvoiceController extends Controller
@@ -19,31 +19,34 @@ class InvoiceController extends Controller
 
     public function generateInvoices()
     {
-        // 'company_id' identifikuje kompaniju kojoj se fakturira
+        // Dohvati sve uspješne poslove koji nisu fakturisani
         $successfulJobs = SuccessfulJob::where('invoiced', 0)
             ->whereMonth('completion_date', '=', Carbon::now()->month)
-            ->get()
-            ->groupBy('company_id');
-    
-        foreach ($successfulJobs as $company_id => $jobs) {
+            ->get();
+
+        // Grupiši poslove po user_id iz bids tabele
+        $groupedJobs = $successfulJobs->mapToGroups(function ($job, $key) {
+            return [$job->bid->user_id => $job];
+        });
+
+        foreach ($groupedJobs as $company_id => $jobs) {
             // Izračunaj ukupan iznos za fakturisanje
             $totalAmountDue = $jobs->sum('amount_due');
-    
+
             // Kreiraj fakturu za kompaniju
             $invoice = new Invoice();
-            $invoice->company_id = $company_id;
+            $invoice->company_id = $company_id; // ovdje koristimo user_id iz bids tabele
             $invoice->amount = $totalAmountDue;
             $invoice->invoice_date = now();
-            $invoice->due_date = now()->addDays(30); // ili neki drugi rok
-            $invoice->status = 'unpaid'; 
+            $invoice->due_date = now()->addDays(30);
+            $invoice->status = 'unpaid';
             $invoice->save();
-    
+
             // Ažuriraj svaki posao kao fakturisan
-            DB::table('successful_jobs')
-                ->whereIn('id', $jobs->pluck('id'))
-                ->update(['invoiced' => 1]);
+            SuccessfulJob::whereIn('id', $jobs->pluck('id'))->update(['invoiced' => 1]);
         }
     }
+
     
 
     public function showInvoices()
