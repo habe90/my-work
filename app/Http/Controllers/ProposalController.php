a0053ef25e22f9fcd7f5b610719c2293d942cf1f
Halil Hodzic <habetech@gmail.com>
2024-04-20 20:44:32 +0200

    Update ProposalController.php

--- app/Http/Controllers/ProposalController.php
+++ app/Http/Controllers/ProposalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Bid;
 use App\Models\Job;
+use App\Models\User;
 use App\Notifications\NewBidPlaced;
 use Illuminate\Support\Facades\Notification;
 
