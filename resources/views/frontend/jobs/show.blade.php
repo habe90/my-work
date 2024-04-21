868855340397fab78646b5435c6c8c88024b5ecf
Halil Hodzic <habetech@gmail.com>
2024-04-21 05:25:26 +0200

    Update show.blade.php

--- resources/views/frontend/jobs/show.blade.php
+++ resources/views/frontend/jobs/show.blade.php
@@ -209,10 +209,11 @@
                                                         <span
                                                             class="badge badge-warning text-white">{{ $bid->status }}</span>
                                                             @if (auth()->id() === $bid->user_id && $bid->edit_count < 3)
-                                                            <button wire:click="$emit('openEditModal', {{ $bid->id }})" class="btn btn-sm btn-secondary">
-                                                                <i class="fa fa-pencil" aria-hidden="true"></i>
-                                                            </button>
-                                                        @endif
+    <button wire:click="edit({{ $bid->id }})" class="btn btn-sm btn-secondary">
+        <i class="fa fa-pencil" aria-hidden="true"></i> 
+    </button>
+@endif
+
                                                         
                                                     </div>
                                                 </div>
